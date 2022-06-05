59433e31206ea752556c6af1370c47434d13d34d
dwyrwas <djwyrwas@gmail.com>
2022-06-05 14:08:38 -0500

    Added willLikelySurvive method; created 30

--- mystery-organism/main.js
+++ mystery-organism/main.js
@@ -15,9 +15,9 @@ const mockUpStrand = () => {
 
 const pAequorFactory = (specimen, dna) => {
   return {
-    specimenNum: specimenNum,
+    specimenNum: specimen,
     dna: dna,
-    mutate: () => {
+    mutate() {
       currentBaseIndex = Math.floor(Math.random()*this.dna.length)
       currentBase = this.dna[currentBaseIndex]
       newBase = returnRandBase()
@@ -27,20 +27,37 @@ const pAequorFactory = (specimen, dna) => {
       this.dna[currentBaseIndex] = newBase
       return this.dna;
     },
-    compareDNA: (pAequor) => {
+    compareDNA(pAequor) {
       let spec1dna = this.dna
       let spec2dna = pAequor.dna
       let samedna = 0
-      for (let i=0; i<spec1dna.length; i++){
+      for (let i = 0; i<15; i++){
         if (spec1dna[i] === spec2dna[i]){
           samedna++
         }
       }
       let percentdna = samedna/spec1dna.length*100
       console.log(`The two specimens have ${percentdna}% DNA in common.`);    
+    },
+    willLikelySurvive(){
+      let goodDNA = 0
+      for (let i=0; i<15; i++){
+        if (this.dna[i] === 'C' || this.dna[i] === 'G'){
+          goodDNA++
+        }
+      }
+      return goodDNA/15*100 >= 60
     }
   }
 };
+
+let sample = []
+for (i=0; i<30; i++){
+  sample.push(pAequorFactory(i, mockUpStrand()))
+};
+
+
+
 
 
 
